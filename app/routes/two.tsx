f5f3752ac716944318bce21326671a2d4b6dee9c
Matt Kane <matt@gatsbyjs.com>
2021-05-25 06:30:18 +0100

    Page 2

--- app/routes/two.tsx
+++ app/routes/two.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-
-export default function Gists() {
+export default function Two() {
   return (
     <div>
       <h2>It's another page!</h2>
-      <p>Hi</p>
+      <p>
+        <a href="/">I've had enough</a>
+      </p>
     </div>
   );
 }
